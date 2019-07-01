5535981ac9b2319eb7bcbb09ed48c925dc1b290b
kuo77122 <kuo77122@gmail.com>
2019-07-01 09:44:41 +0000

    add jupyter-config.py

---
+++ jupyterhub/jupyter-config.py
@@ -0,0 +1,17 @@
+import os
+from oauthenticator.github import GitHubOAuthenticator
+
+c.JupyterHub.authenticator_class = GitHubOAuthenticator
+
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_IMAGE']
+c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+c.JupyterHub.hub_ip = os.environ['HUB_IP']
+
+c.JupyterHub.services = [
+    {
+        'name': 'cull-idle',
+        'admin': True,
+        'command': [sys.executable, 'cull_idle_servers.py', '--timeout=3600'],
+    }
+]
